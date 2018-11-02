89c1d58426191d74585ce9698e7a1cf0b3101f27
Miro <huangj14@chinatelecom.cn>
2018-11-02 10:52:49 +0800

    时间改为用momentjs, 加载数据为空显示暂无数据

--- pages/components/displayInfo/displayInfo.js
+++ pages/components/displayInfo/displayInfo.js
@@ -1,13 +1,16 @@
-//home.js
 //获取应用实例
 const app = getApp();
+import moment from '../../../js/moment.min.js'
+
+
 
 Component({
   data: {
     infos:[],
     title:['紧急寻找', '我的发布'],
     selectedType:'全部',
-    filterInfos:[]
+    filterInfos:[],
+    loaded:false,
   },
   properties: {
     queryType:     Number,
@@ -49,22 +52,20 @@ Component({
     queryHelpList: function(queryConds) {
       app.HttpService.queryHelpList(queryConds).then(
         (res)=>{
-
-          res.data.forEach((item)=>{
-              let originArr = item.picUrls.concat()
-              let picStr = originArr && originArr.length && originArr[0];
-              item.picUrls = picStr && picStr.split(',')
-              item.publishTime = this.formatMsgTime(parseInt(item.createTime))
-          })
-          // console.log(res.data)
-          this.setData({
-            infos: res.data,
-            filterInfos: res.data
-          })
-          // // 保存数据
-          // res.data.forEach(info => {
-          //   app.globalData.lostInfos[info._id] = info
-          // });
+          if(res.data && res.data.length){
+            res.data.forEach((item)=>{
+                let originArr = item.picUrls.concat()
+                let picStr = originArr && originArr.length && originArr[0];
+                item.picUrls = picStr && picStr.split(',')
+                item.publishTime = moment(parseInt(item.createTime)).fromNow()
+            })
+            // console.log(res.data)
+            this.setData({
+              infos: res.data,
+              filterInfos: res.data,
+              loaded: true,
+            })
+          }
         }
       ) 
     },
@@ -96,30 +97,5 @@ Component({
           }
         })
     },
-    formatMsgTime (stamptime) {
-      var current_time = + new Date();
-      var diff = (current_time - stamptime)/1000;
-      
-      var agoAt = '刚刚';
-      var timePoints = [
-          { value: 60 * 60 * 24 * 365, suffix: '年前', max: 2 },
-          { value: 60 * 60 * 24 * 30,  suffix: '月前', max: 11 },
-          { value: 60 * 60 * 24 * 7,   suffix: '周前', max: 4 },
-          { value: 60 * 60 * 24,       suffix: '天前', max: 6 },
-          { value: 60 * 60,            suffix: '小时前', max: 23 },
-          { value: 60 * 1,            suffix: '分钟前',  max: 59 }
-      ];
-
-      for (var i = 0; i < timePoints.length; i++) {
-        var point = timePoints[i];
-        var mode = Math.floor(diff / point.value);
-        if (mode >= 1) {
-            agoAt = Math.min(mode, point.max) + point.suffix;
-            break;
-        }
-      }
-      return agoAt;
-    },
   },
- 
 });
